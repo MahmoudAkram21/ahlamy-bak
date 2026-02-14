1f321d9cbfbeab2621d397a524b0b71b34871904
MahmoudAkram21 <mahmoudakram422@gmail.com>
2026-02-14 14:17:48 +0200

    last update

---
+++ ecosystem.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+    apps: [
+      {
+        name: "ahlamy",
+        script: "npm",
+        args: "start",
+        env: {
+          PORT: 6000,
+          NODE_ENV: "production",
+        },
+      },
+    ],
+  };
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
